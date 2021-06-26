7526c68c908092daa2e857c86fc6aabea7b94666
mess-ier <chad.bjornberg@gmail.com>
2021-06-25 20:46:20 -0700

    added wip words_to_type

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ class Typer(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.title("Typer")
-        self.geometry("610x210")
+        self.geometry("850x380")
         main_frame = Frame(self)
         self.frames = {
             "typer": TypeFrame(self),
-            "words": WordsFrame(self),
             "results": ResultsFrame(self)
         }
 
@@ -23,11 +22,10 @@ class Typer(Tk):
     def show_frame(self, name):
         frame = self.frames[name]
         frame.config(bg="#f1f1f1")
-        frame.place(width=600, height=200, relx=0.5, rely=0.5, anchor=CENTER)
+        frame.place(width=850, height=380, relx=0.5, rely=0.5, anchor=CENTER)
         frame.tkraise()
 
     def show_typer_statistics(self):
-        self.show_frame("words")
         self.show_frame("typer")
 
     def show_results_frame(self):
@@ -58,20 +56,24 @@ class TypeFrame(Frame):
         restart_button_text.set("Restart")
         Button(self, textvariable=restart_button_text, fg="red", relief=FLAT).grid(row=0, column=6)
 
+        words_txt = pd.read_csv("words.txt")
+        words_csv = words_txt.to_csv("word.csv", index=None)
+        words_to_type = Text(self).grid(row=1, column=0, columnspan=6)
+        with open(words_csv, "r") as file:
+            words = file.read()
+            words_to_type.insert("1.0", words)
+
         # row 2
         typing_area_text = StringVar()
         typing_area_text.set("type the words here")
-        Entry(self, textvariable=typing_area_text).grid(row=2, column=0, columnspan=6)
+        Entry(self, textvariable=typing_area_text, width=80).grid(row=2, column=0, columnspan=6)
 
 
 class WordsFrame(Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        words = pd.read_csv("words.txt").values()
-
-        frame = Frame(self, bg="#ffffff")
-        Label(self, text=words).tkraise()
+        pass
 
 
 class ResultsFrame(Frame):
